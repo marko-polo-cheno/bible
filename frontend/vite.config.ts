5aa0bb5b7d9d4874749ab5712d39c8705df09da1
Mark Chen <markzchen1@gmail.com>
2024-12-26 11:27:16 -0500

    hmm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['assets/*'],
-  server: {
-    port: 5173,
-  },
-  preview: {
-    port: 5173,
-  },
-  build: {
-    sourcemap: process.env.WITH_SOURCE_MAPS === 'true',
-  },
+  base: '/bible/' // Add this line for GitHub Pages deployment
 });
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['assets/*'],
+//   server: {
+//     port: 5173,
+//   },
+//   preview: {
+//     port: 5173,
+//   },
+//   build: {
+//     sourcemap: process.env.WITH_SOURCE_MAPS === 'true',
+//   },
+// });
