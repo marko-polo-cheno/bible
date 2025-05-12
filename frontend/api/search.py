1a9e065148968226ecd8565b4d0c6f1c42e5bc0a
Mark Chen <markzchen1@gmail.com>
2025-05-12 00:51:32 -0400

    ok

--- frontend/api/search.py
+++ frontend/api/search.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://marko-polo-cheno.github.io", "https://bible-search-wheat.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,21 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bible/' // Add this line for GitHub Pages deployment
+  base: './' // Add '/bible/ for GitHub Pages deployment
 });
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ['assets/*'],
-//   server: {
-//     port: 5173,
-//   },
-//   preview: {
-//     port: 5173,
-//   },
-//   build: {
-//     sourcemap: process.env.WITH_SOURCE_MAPS === 'true',
-//   },
-// });
